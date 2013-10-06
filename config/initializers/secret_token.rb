530d435dfaaee2fd01b2975fcfe2d10c22bead69
Raphael Costa <raphael@raphaelcosta.net>
2013-10-06 02:39:39 -0300

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KeynoteJs::Application.config.secret_key_base = 'f48b0923e83d397e3a3a882c58389ced4fc433a6cb021ee452809b699e30ed357d3391ac6de93bfd2fe7155df6f41f938bb279779c2b4cfbcd85b50778c3b5d9'
